652d724007810d56e9b7d34ba86d9feb4ea1d324
manulpzz <93796987+manulpzz@users.noreply.github.com>
2024-06-17 20:50:27 -0300

    .

---
+++ pages/api/manufacturers.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default async function handler(request, response) {
+  const data = []
+
+  if (request.method === "GET") {
+    const { _end: limit, _start: offset, id } = request.query
+
+    const where = {}
+    if (id) {
+      if (Array.isArray(id)) {
+        where.id = { in: id.map(String) }
+      } else {
+        where.id = id
+      }
+    }
+
+    const items = await prisma.manufacturers.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+      where,
+      orderBy: {
+        id: "asc",
+      },
+      take: limit ? Number(limit) : undefined,
+      skip: offset ? Number(offset) : undefined,
+    })
+
+    items.forEach(element => {
+      data.push(element)
+    })
+  } else if (request.method === "POST") {
+    const { id, name } = request.body
+    await prisma.manufacturers.create({
+      data: {
+        id,
+        name,
+      },
+    })
+  }
+
+  return response.status(200).json(data)
+}
